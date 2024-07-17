082aa98ef4fdeec24e8a53e4e225fad5b2ebb5ab
layzasilva <kermilynlay@gmail.com>
2024-07-17 03:57:20 -0300

    3º commit - adicionando comentários e implementando objeto Scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //criando o objeto scanner 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Desafio da DIO - Conta bancária via terminal
+
+        //Criando as variáveis
+
         int numero = Integer.valueOf(args[0]);
+        //armazena o número da agência do cliente
         String nomeCliente = args[1];
+        //armazena o nome do cliente
         String agencia = args[2];
+        //armazena o nome da agência do cliente
         double saldo = Integer.valueOf(args[3]);
+        //armazena o saldo que o cliente possui
     }
 }
