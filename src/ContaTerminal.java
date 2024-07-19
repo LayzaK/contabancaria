113f6994de578d3edc394d93fad5f11a45c68483
layzasilva <kermilynlay@gmail.com>
2024-07-19 08:20:45 -0300

    5º commit - adicionando comentários importantes

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,15 +11,19 @@ public class ContaTerminal {
         //Criando as variáveis e utilizando o Scanner para fazer uma interação com o usuário
 
         System.out.println("Digite seu nome: ");
+        //a variável reserva o nome do cliente e o objeto scanner permite a interação
         String nomeCliente = scanner.next();
 
         System.out.println("Digite o número da sua agência: ");
+        //a variável reserva o número da agência e o objeto scanner permite a interação
         int numeroAgencia = scanner.nextInt();
 
         System.out.println("Digite o nome da sua agência: ");
+        //a variável reserva o nome da agência e o objeto scanner permite a interação
         var agência = scanner.next();
 
         System.out.println("Digite o seu saldo: ");
+        //a variável reserva o saldo do cliente e o objeto scanner permite a interação
         double saldo = scanner.nextInt();
 
         //imprimindo os dados obtidos pelo usuário
